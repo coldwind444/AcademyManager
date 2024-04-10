33dc3ad62eb0177fadebf7b18d7e82566b91a36c
Đạt Phạm <phamtuandat7704@gmail.com>
2024-04-10 22:50:05 +0700

    add file

---
+++ Viewmodels/CourseResultVM.cs
@@ -0,0 +1,14 @@
+﻿using AcademyManager.Viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyManager.Viewmodels
+{
+    public class CourseResultVM : BaseViewModel
+    {
+        public CourseResultVM() { }
+    }
+}
--- Viewmodels/EnrolmentVM.cs
+++ Viewmodels/EnrolmentVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AcademyManager.Viewmodel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace AcademyManager.Viewmodels
 {
-    internal class Class1
+    public class EnrolmentVM : BaseViewModel
     {
+
+        public EnrolmentVM() { }
     }
 }
