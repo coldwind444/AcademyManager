c777ebd4d1ac28e56905b31bc79c792354611c71
coldwind444 <minhtan14042004@gmail.com>
2024-04-11 08:34:14 +0700

    Create this class to save Course data

---
+++ Models/ClassIdentifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyManager.Models
+{
+    public class ClassIdentifier
+    {
+        public string TermID { get; set; }
+        public string CourseID { get; set; }
+        public string ClassID { get; set; }
+        public ClassIdentifier(string termid, string courseid, string classid)
+        {
+            TermID = termid;
+            CourseID = courseid;
+            ClassID = classid;
+        }
+    }
+}
