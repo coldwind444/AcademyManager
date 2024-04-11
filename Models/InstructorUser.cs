265276d75c7de6eeb1e31617f2b750716246492d
coldwind444 <minhtan14042004@gmail.com>
2024-04-11 10:38:31 +0700

    Implement UpdateScore method

--- Models/InstructorUser.cs
+++ Models/InstructorUser.cs
@@ -10,13 +10,14 @@ namespace AcademyManager.Models
     {
         public string Certificate { get; set; }
         public string Speciality { get; set; }
-        public void CreateClass(string termID, string courseID, string id, string name, TimeOnly bgT, TimeOnly eT, DateOnly bgD, DateOnly eD, string room)
+        public async Task UpdateScore(string termID, string courseID, string classID, string studentID, int field, double score)
         {
-
-        }
-        public void UpdateScore(string termID, string courseID, string classID, string studentID, int field, double score)
-        {
-
+            DatabaseManager database = new DatabaseManager();
+            Term term = await database.GetTermAsync(termID);
+            if (field == 1) term.Courses[courseID].Classes[classID].Students[studentID].DailyTestScore = score;
+            else if (field == 2) term.Courses[courseID].Classes[classID].Students[studentID].Mid_Term = score;
+            else term.Courses[courseID].Classes[classID].Students[studentID].Final = score;
+            await database.UpdateTermAsync(term);
         }
         public InstructorUser(string id, string fullname, string email, DateOnly birthday, string faculty, string cert, string spec) 
             : base(id, fullname, email, birthday, faculty)
