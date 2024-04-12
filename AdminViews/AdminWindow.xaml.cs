b5a10da242a4b038230ca37db71a39b78b59e973
coldwind444 <minhtan14042004@gmail.com>
2024-04-12 21:03:03 +0700

    Recover AdminWindow code-behind

---
+++ AdminViews/AdminWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using AcademyManager.AdminViewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AcademyManager.AdminViews
+{
+    public partial class AdminWindow : Window
+    {
+        public AdminWindowVM Viewmodel {  get; set; }
+        public AdminWindow()
+        {
+            this.DataContext = Viewmodel = new AdminWindowVM();
+            InitializeComponent();
+        }
+    }
+}
