7c8296a106f6d83a29903bb9b28aebcbb5dccef8
Nguyen Thuy Dung <164638079+thuydung05@users.noreply.github.com>
2024-04-17 22:54:35 +0700

    Add files via upload

---
+++ MainStudentScreen.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcademyManager.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MainScreen.xaml
+    /// </summary>
+    public partial class MainStudentScreen : UserControl
+    {
+        public MainStudentScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void adminWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
