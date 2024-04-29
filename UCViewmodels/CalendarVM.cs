ac1aac8f759c42713a7eb510c4811ad6165c0d1e
coldwind444 <minhtan14042004@gmail.com>
2024-04-29 19:51:18 +0700

    add viewmodel files

---
+++ UCViewmodels/CalendarVM.cs
@@ -0,0 +1,14 @@
+﻿using AcademyManager.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyManager.UCViewmodels
+{
+    public class CalendarVM : BaseViewModel
+    {
+        public CalendarVM() { }
+    }
+}
