19647a3e675011579ad410ae8bb094f7271c4192
coldwind444 <minhtan14042004@gmail.com>
2024-05-01 22:50:15 +0700

    add student name property

--- Models/StudentRecord.cs
+++ Models/StudentRecord.cs
@@ -8,12 +8,16 @@ namespace AcademyManager.Models
 {
     public class StudentRecord
     {
+        public string ID { get; set; }
+        public string Name { get; set; }
         public double DailyTestScore { get; set; }
         public double Mid_Term { get; set; }
         public double Final {  get; set; }
         public double GPA { get; set; }
-        public StudentRecord(string studentID, double dailyTestScore, double mid_Term, double final, double gPA)
+        public StudentRecord(string studentID, string name, double dailyTestScore, double mid_Term, double final, double gPA)
         {
+            ID = studentID;
+            Name = name;
             DailyTestScore = dailyTestScore;
             Mid_Term = mid_Term;
             Final = final;
