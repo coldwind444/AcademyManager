a7ca0be0549ad524adaad0521a492a86f49b2111
coldwind444 <minhtan14042004@gmail.com>
2024-05-04 16:22:19 +0700

    add customed message box to show notification of connection failure

---
+++ UCViews/CustomedMessageBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AcademyManager.UCViews
+{
+    /// <summary>
+    /// Interaction logic for CustomedMessageBox.xaml
+    /// </summary>
+    public partial class CustomedMessageBox : Window
+    {
+        public CustomedMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            App.Current.Shutdown();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
